57e0eca9a21ff6b2c2f4897d629a474878478e6f
OmarJouied <omarjouied1421@yahoo.com>
2023-05-14 19:47:48 +0100

    search the 500 server error 3

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,11 @@
 export const GET = async (req) => {
-    // const { country } = await req.json();
+    try {
+        const { country } = await req.json();
 
-    if (!country) return new Response(JSON.stringify(["World", "Morocco", "Algeria"])); // epsg.map(item => item.country)
+        if (!country) return new Response(JSON.stringify(["World", "Morocco", "Algeria"])); // epsg.map(item => item.country)
 
-    return new Response(JSON.stringify(["1", "2", "3"])); // epsg.filter(item => item.country === country)
+        return new Response(JSON.stringify(["1", "2", "3"])); // epsg.filter(item => item.country === country)
+    } catch (error) {
+        return new Response(error, { status: 500 });
+    }
 }
